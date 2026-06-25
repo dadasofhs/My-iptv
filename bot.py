42921bcd1c83cb036938129c6fd9685f4a3e1eb0
dadasofhs <omerdadasov88@gmail.com>
2026-06-25 20:10:00 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,22 @@
-# Hər hansı bir saytı skan etməyə ehtiyac yoxdur, birbaşa rəsmi və açıq IP-ləri yazırıq
+# Həqiqətən hər yerdə (həm hlsplayer-də, həm TV-də) açıq olan rəsmi yayım linkləri
 channels = [
-    # --- SƏNƏDLİ FİLM (BELGESEL) ---
-    {"name": "TRT Belgesel HD", "link": "https://trt.canlitvme.com/trtbelgesel/trtbelgesel/playlist.m3u8", "group": "Belgesel"},
-    {"name": "DMAX HD (Yedek)", "link": "https://cdn508.canlitvme.com/dmax/dmax/playlist.m3u8", "group": "Belgesel"},
-    {"name": "TLC HD (Yedek)", "link": "https://cdn508.canlitvme.com/tlc/tlc/playlist.m3u8", "group": "Belgesel"},
-
-    # --- ÜMUMİ KANALLAR ---
-    {"name": "TRT 1 HD", "link": "https://trt.canlitvme.com/trt1/trt1/playlist.m3u8", "group": "Ulusal"},
-    {"name": "ATV HD", "link": "https://atv.canlitvme.com/atv/atv/playlist.m3u8", "group": "Ulusal"},
-    {"name": "Kanal D HD", "link": "https://kanald.canlitvme.com/kanald/kanald/playlist.m3u8", "group": "Ulusal"},
-    {"name": "Star TV HD", "link": "https://star.canlitvme.com/star/star/playlist.m3u8", "group": "Ulusal"},
-    {"name": "Show TV HD", "link": "https://show.canlitvme.com/show/show/playlist.m3u8", "group": "Ulusal"},
-    {"name": "TV8 HD", "link": "https://tv8.canlitvme.com/tv8/tv8/playlist.m3u8", "group": "Ulusal"},
-
-    # --- İDMAN VƏ XƏBƏR ---
-    {"name": "TRT Spor HD", "link": "https://trt.canlitvme.com/trtspor/trtspor/playlist.m3u8", "group": "Spor"},
-    {"name": "A Spor HD", "link": "https://aspor.canlitvme.com/aspor/aspor/playlist.m3u8", "group": "Spor"},
-    {"name": "TRT Haber HD", "link": "https://trt.canlitvme.com/trthaber/trthaber/playlist.m3u8", "group": "Haber"},
-    {"name": "NTV HD", "link": "https://ntv.canlitvme.com/ntv/ntv/playlist.m3u8", "group": "Haber"},
-    {"name": "CNN Türk HD", "link": "https://cnnturk.canlitvme.com/cnnturk/cnnturk/playlist.m3u8", "group": "Haber"}
+    # --- AZƏRBAYCAN KANALLARI (Açıq və Stabil) ---
+    {"name": "İctimai TV (İTV)", "link": "https://stream.itv.az/itv/itv.m3u8", "group": "Azərbaycan"},
+    {"name": "Xəzər TV", "link": "https://live.xazar.az/hls/stream.m3u8", "group": "Azərbaycan"},
+    {"name": "CBC Azerbaijan", "link": "https://stream.cbc.az/cbc/cbc.m3u8", "group": "Azərbaycan"},
+    
+    # --- TRT RƏSMİ LİNKƏRİ (Heç bir pleyerdə qapanmayan rəsmi CDN yolları) ---
+    {"name": "TRT Belgesel HD", "link": "https://trt.live.enetres.net/live/trtbelgesel.m3u8", "group": "Belgesel"},
+    {"name": "TRT 1 HD", "link": "https://trt.live.enetres.net/live/trt1.m3u8", "group": "Ulusal"},
+    {"name": "TRT Spor HD", "link": "https://trt.live.enetres.net/live/trtspor.m3u8", "group": "Spor"},
+    {"name": "TRT Haber HD", "link": "https://trt.live.enetres.net/live/trthaber.m3u8", "group": "Haber"},
+    {"name": "TRT Avaz HD", "link": "https://trt.live.enetres.net/live/trtavaz.m3u8", "group": "Ulusal"},
+    {"name": "TRT Müzik", "link": "https://trt.live.enetres.net/live/trtmuzik.m3u8", "group": "Müzik"},
+    
+    # --- DİGƏR AÇIQ TÜRK KANALLARI ---
+    {"name": "HaberTürk HD", "link": "https://ciner-live.daioncdn.net/haberturk/haberturk.m3u8", "group": "Haber"},
+    {"name": "Show TV HD", "link": "https://ciner-live.daioncdn.net/showtv/showtv.m3u8", "group": "Ulusal"},
+    {"name": "Bloomberg HT", "link": "https://ciner-live.daioncdn.net/bloomberght/bloomberght.m3u8", "group": "Haber"}
 ]
 
 m3u_text = "#EXTM3U\n"
@@ -30,4 +28,5 @@ for ch in channels:
 with open("tv.m3u", "w", encoding="utf-8") as f:
     f.write(m3u_text)
 
-print("tv.m3u faylı birbaşa və bloklanmayan rəsmi linklərlə yeniləndi!")
+print("tv.m3u faylı tam rəsmi və bloklanmayan yayım şəbəkəsi ilə yeniləndi!")
+
